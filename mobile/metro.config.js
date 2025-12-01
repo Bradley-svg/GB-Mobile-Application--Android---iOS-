7b8f201895c4d91a4d88595a7a3281017e07ea90
Bradley-svg <bradleyayliffl@gmail.com>
2025-12-01 17:25:59 +0200

    committ

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,9 +1,10 @@
+// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const config = getDefaultConfig(__dirname);
 
-// Ignore the corrupted backup folders so Metro doesn't choke on them when walking the tree.
-config.resolver.blockList = exclusionList([/node_modules_old.*/]);
+// Exclude the broken backup folder from Metro so it never scans it
+config.resolver.blockList = exclusionList([/node_modules_old\/.*/]);
 
 module.exports = config;
